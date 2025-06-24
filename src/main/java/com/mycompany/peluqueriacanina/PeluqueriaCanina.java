3c6bf20c296830ab09ef3e00267323fe54ef9d34
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2025-06-23 22:51:12 -0300

    prueba

--- src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
+++ src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -7,7 +7,7 @@ import com.mycompany.peluqueriacanina.igu.Principal;
 public class PeluqueriaCanina {
 
     public static void main(String[] args) {
-        
+        //creo instancia de pantalla principal
         Principal princ = new Principal();
         princ.setVisible(true);
         princ.setLocationRelativeTo(null);
